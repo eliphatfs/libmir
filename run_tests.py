5afc545503fec59d3536aed8ac06f6fa8aa6f505
eliphatfs <flandre@scarletx.cn>
2021-09-20 19:25:08 +0800

    Testing for different python versions

--- run_tests.py
+++ run_tests.py
@@ -4,8 +4,10 @@ Created on Fri Mar 26 16:20:34 2021
 
 @author: eliphat
 """
+import sys
 import unittest
 
 
+print("Python", sys.version)
 __unittest = True
 unittest.main(module=None, argv=['unittest', 'discover', '-v'])
